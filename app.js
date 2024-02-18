d41624b40954726d853e0433961e6b144878cefe
beacarretie <bcarretie@gmail.com>
2024-02-18 20:34:11 +0100

    redirects

---
+++ app.js
@@ -0,0 +1,9 @@
+import "style.css";
+
+    <div class="app">
+        <h1 class="title">Mastermind</h1>
+        <div class="imageContainer">
+            <img id="backgroundHome" src="./imgs/backgroundHome.jpg" alt="backgroundHome">
+        </div>
+    </div>
+</body>
